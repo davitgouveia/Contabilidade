d4b8ed3c64455ae65deceab47e6beb0ec5827450
Davi Gouveia <75867613+davitgouveia@users.noreply.github.com>
2022-07-25 11:08:45 -0300

    Update script_contabilidade.sql

--- script_contabilidade.sql
+++ script_contabilidade.sql
@@ -10,41 +10,56 @@ CREATE SCHEMA tb, vw;
 --Criação de tabelas
 
 CREATE TABLE tb.usuario (
-	idUser INT,
+	idUser INT PRIMARY KEY AUTOINCREMENT,
 	nomeUser VARCHAR(100)
 );
 
-CREATE TABLE tb.renda (
-	idRenda INT,
-	nomeRenda VARCHAR (100),
-	descRenda VARCHAR (255),
-	valorRenda FLOAT,
-	dataRenda DATE,
-	idRecebedor INT
-);
-
 CREATE TABLE tb.compra (
-	idCompra INT,
-	nomeCompra VARCHAR (100),
+	idCompra INT PRIMARY KEY AUTOINCREMENT,
+	tipoCompra VARCHAR (100),
 	descCompra VARCHAR (255),
 	tipoPagCompra INT,
 	valorCompra FLOAT,
 	dataCompra DATE,
 	idComprador INT
+	CONSTRAINT fk_UserCompra FOREIGN KEY (idComprador) REFERENCES tb.usuario (idUser)
+	CONSTRAINT fk_TipoCompra FOREIGN KEY (tipoCompra) REFERENCES tb.tipoPagamento (nomeTipoCompra)
+	CONSTRAINT fk_TipoPagCompra FOREIGN KEY (tipoPagCompra) REFERENCES tb.tipoPagamento (nomeTipoPag)
 );
 
 CREATE TABLE tb.conta (
-	idConta INT,
-	nomeConta VARCHAR (100),
+	idConta INT PRIMARY KEY AUTOINCREMENT,
+	tipoConta VARCHAR (100),
 	descConta VARCHAR (255),
 	valorConta FLOAT,
 	dataAtribConta DATE,
 	dataVencConta DATE,
 	dataPagConta DATE,
-	statusConta VARCHAR 10
+	statusConta VARCHAR (10)
+	tipoPagCompra VARCHAR (50)
 	idPagador INT
-	
+	CONSTRAINT fk_UserPaga FOREIGN KEY (idPagador) REFERENCES tb.usuario (idUser)
+	CONSTRAINT fk_TipoPagConta FOREIGN KEY (tipoPagConta) REFERENCES tb.tipoPagamento (nomeTipoPag)
 	constraint statusConta check((statusConta = 'PENDENTE') or (statusConta = 'PAGO') or (statusConta = 'ATRASADO'))
 	
 );
-	
+
+CREATE TABLE tb.renda (
+	idRenda INT PRIMARY KEY AUTOINCREMENT,
+	nomeRenda VARCHAR (100),
+	descRenda VARCHAR (255),
+	valorRenda FLOAT,
+	dataRenda DATE,
+	idRecebedor INT
+	CONSTRAINT fk_UserRecebe FOREIGN KEY (idRecebedor) REFERENCES tb.usuario (idUser)
+);
+
+CREATE TABLE tb.tipoPagamento (
+	idPag INT
+	nomeTipoPag VARCHAR (50)
+);
+
+CREATE TABLE tb.tipoCompra (
+	idCompra INT
+	nomeTipoCompra VARCHAR (100)
+);	
