3de711623a852f7267f93636071bf256d9a2037d
Davi Gouveia <75867613+davitgouveia@users.noreply.github.com>
2022-07-27 10:56:57 -0300

    Update script_contabilidade.sql

--- script_contabilidade.sql
+++ script_contabilidade.sql
@@ -5,61 +5,86 @@ CREATE DATABASE Contabilidade;
 --Criação de esquemas
 --tb: tabelas , vw: visoes
 
-CREATE SCHEMA tb, vw;
+CREATE SCHEMA tb;
+CREATE SCHEMA vw;
 
 --Criação de tabelas
 
 CREATE TABLE tb.usuario (
-	idUser INT PRIMARY KEY AUTOINCREMENT,
+	idUser INT PRIMARY KEY,
 	nomeUser VARCHAR(100)
 );
 
 CREATE TABLE tb.compra (
-	idCompra INT PRIMARY KEY AUTOINCREMENT,
+	idCompra INT PRIMARY KEY,
 	tipoCompra VARCHAR (100),
 	descCompra VARCHAR (255),
-	tipoPagCompra INT,
+	tipoPagCompra VARCHAR (50),
 	valorCompra FLOAT,
 	dataCompra DATE,
 	idComprador INT
-	CONSTRAINT fk_UserCompra FOREIGN KEY (idComprador) REFERENCES tb.usuario (idUser)
-	CONSTRAINT fk_TipoCompra FOREIGN KEY (tipoCompra) REFERENCES tb.tipoPagamento (nomeTipoCompra)
-	CONSTRAINT fk_TipoPagCompra FOREIGN KEY (tipoPagCompra) REFERENCES tb.tipoPagamento (nomeTipoPag)
 );
 
 CREATE TABLE tb.conta (
-	idConta INT PRIMARY KEY AUTOINCREMENT,
+	idConta INT PRIMARY KEY,
 	tipoConta VARCHAR (100),
 	descConta VARCHAR (255),
 	valorConta FLOAT,
 	dataAtribConta DATE,
 	dataVencConta DATE,
 	dataPagConta DATE,
-	statusConta VARCHAR (10)
-	tipoPagCompra VARCHAR (50)
+	statusConta VARCHAR (10),
+	tipoPagConta VARCHAR (50),
 	idPagador INT
-	CONSTRAINT fk_UserPaga FOREIGN KEY (idPagador) REFERENCES tb.usuario (idUser)
-	CONSTRAINT fk_TipoPagConta FOREIGN KEY (tipoPagConta) REFERENCES tb.tipoPagamento (nomeTipoPag)
-	constraint statusConta check((statusConta = 'PENDENTE') or (statusConta = 'PAGO') or (statusConta = 'ATRASADO'))
-	
+		
 );
 
 CREATE TABLE tb.renda (
-	idRenda INT PRIMARY KEY AUTOINCREMENT,
+	idRenda INT PRIMARY KEY,
 	nomeRenda VARCHAR (100),
 	descRenda VARCHAR (255),
 	valorRenda FLOAT,
 	dataRenda DATE,
 	idRecebedor INT
-	CONSTRAINT fk_UserRecebe FOREIGN KEY (idRecebedor) REFERENCES tb.usuario (idUser)
+	
+);
+
+CREATE TABLE tb.divida (
+	idDivida INT PRIMARY KEY,
+	tipoDivida VARCHAR (100),
+	descDivida VARCHAR (255),
+	valorDivida FLOAT,
+	dataDivida DATE,
+	idDevedor INT,
+	idCredor INT
+	
 );
 
 CREATE TABLE tb.tipoPagamento (
-	idPag INT
-	nomeTipoPag VARCHAR (50)
+	idPag INT,
+	nomeTipoPag VARCHAR (50) UNIQUE
 );
 
 CREATE TABLE tb.tipoCompra (
-	idCompra INT
-	nomeTipoCompra VARCHAR (100)
+	idCompra INT,
+	nomeTipoCompra VARCHAR (100) UNIQUE
 );	
+
+-- Restrições
+ALTER TABLE tb.compra
+ADD CONSTRAINT fk_UserCompra FOREIGN KEY (idComprador) REFERENCES tb.usuario (idUser),
+ADD CONSTRAINT fk_TipoCompra FOREIGN KEY (tipoCompra) REFERENCES tb.tipoCompra (nomeTipoCompra),
+ADD CONSTRAINT fk_TipoPagCompra FOREIGN KEY (tipoPagCompra) REFERENCES tb.tipoPagamento (nomeTipoPag);
+
+
+ALTER TABLE tb.conta
+ADD CONSTRAINT fk_UserPaga FOREIGN KEY (idPagador) REFERENCES tb.usuario (idUser),
+ADD CONSTRAINT fk_TipoPagConta FOREIGN KEY (tipoPagConta) REFERENCES tb.tipoPagamento (nomeTipoPag),
+ADD CONSTRAINT statusConta CHECK((statusConta = 'PENDENTE') OR (statusConta = 'PAGO') OR (statusConta = 'ATRASADO'));
+
+ALTER TABLE tb.renda
+ADD CONSTRAINT fk_UserRecebe FOREIGN KEY (idRecebedor) REFERENCES tb.usuario (idUser);
+
+ALTER TABLE tb.divida
+ADD CONSTRAINT fk_UserDeve FOREIGN KEY (idDevedor) REFERENCES tb.usuario (idUser),
+ADD CONSTRAINT fk_UserCredor FOREIGN KEY (idCredor) REFERENCES tb.usuario (idUser);
